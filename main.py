a2f1be11275ae533c01260d35ab4784939386ede
zzaid17 <zaidhoda@gmail.com>
2023-05-08 03:59:29 -0600

    Broke some things

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from load_data import load_ephemeris, load_star_data
-from utils import np, sf, plt, ps, dt, load, tzwhere, timezone, utc, wgs84, Star, build_stereographic_projection
+from utils import np, sf, plt, ps, datetime, load, tzwhere, timezone, utc, wgs84, Star, build_stereographic_projection
 
 ephemeris = load_ephemeris()
 star_data = load_star_data()
 local_position = [51.0447, 114.0719]
-time = dt.now()
+time = datetime.now()
 
-local_timezone_string = tzwhere.tzwhere().tzNameAt(local_position[0], local_position[1])
-local_timezone = timezone(local_timezone_string)
-utc_dt = local_timezone.localize(dt, is_dst=None).astimezone(utc)
+#local_timezone_string = tzwhere.tzwhere().tzNameAt(local_position[0], local_position[1])
+#local_timezone = timezone('UTC')
+#utc_dt = local_timezone.localize(datetime, is_dst=None).astimezone(utc)
 
-time = load.timescale().from_datetime(utc_dt)
+#time = load.timescale().from_datetime(utc_dt)
 observer = wgs84.latlon(latitude_degrees=local_position[0], longitude_degrees=local_position[1]).at(time)
 
 sun, earth = ephemeris['sun'], ephemeris['earth']
@@ -24,5 +24,9 @@ projection = build_stereographic_projection(center)
 star_positions = earth.at(time).observe(Star.from_dataframe(star_data))
 star_data['x'], star_data['y'] = projection(star_positions)
 
+# Star plot
 
+star_plot = plt.plot(figsize = (20,20), facecolor='#041A40')
+
+plt.show()
 
--- utils.py
+++ utils.py
@@ -2,11 +2,13 @@ import numpy as np
 import skyfield as sf
 import matplotlib.pyplot as plt
 import pandas as ps
-import datetime as dt
+from datetime import datetime
+
 
 from skyfield.api import load, wgs84, Star
 from skyfield.data import hipparcos
 from skyfield.projections import build_stereographic_projection
 
 from tzwhere import tzwhere
+
 from pytz import timezone, utc
